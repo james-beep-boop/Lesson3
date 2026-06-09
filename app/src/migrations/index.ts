5eeee38712cbab4f4fd5396f452c00501ee46b1f
James BeepBoop <63834492+james-beep-boop@users.noreply.github.com>
2026-06-09 09:49:37 -0700

    migrate: subject_grades compound-unique index; drop media table

--- app/src/migrations/index.ts
+++ app/src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20260608_024132_initial from './20260608_024132_initial';
 import * as migration_20260608_145602_lesson_entities from './20260608_145602_lesson_entities';
 import * as migration_20260608_224715_bundle_versioning from './20260608_224715_bundle_versioning';
+import * as migration_20260609_164927_subjectgrade_unique_drop_media from './20260609_164927_subjectgrade_unique_drop_media';
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20260608_224715_bundle_versioning.up,
     down: migration_20260608_224715_bundle_versioning.down,
-    name: '20260608_224715_bundle_versioning'
+    name: '20260608_224715_bundle_versioning',
+  },
+  {
+    up: migration_20260609_164927_subjectgrade_unique_drop_media.up,
+    down: migration_20260609_164927_subjectgrade_unique_drop_media.down,
+    name: '20260609_164927_subjectgrade_unique_drop_media'
   },
 ];
